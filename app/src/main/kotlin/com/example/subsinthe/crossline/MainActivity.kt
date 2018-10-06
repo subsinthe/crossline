4494bbee599b06b4cc4137ee2f51dc2935d760c4
subsinthe <subsinthe@gmail.com>
2018-10-07 00:11:18 +0300

    Establish global default exception handler

--- app/src/main/kotlin/com/example/subsinthe/crossline/MainActivity.kt
+++ app/src/main/kotlin/com/example/subsinthe/crossline/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.subsinthe.crossline.network.ISocketFactory
 import com.example.subsinthe.crossline.soulseek.Credentials
 import com.example.subsinthe.crossline.util.AndroidLoggingHandler
 import com.example.subsinthe.crossline.util.loggerFor
+import kotlinx.coroutines.experimental.CoroutineExceptionHandler
 import kotlinx.coroutines.experimental.CoroutineScope
 import kotlinx.coroutines.experimental.Dispatchers
 import kotlinx.coroutines.experimental.IO
@@ -26,12 +27,23 @@ import kotlin.coroutines.experimental.CoroutineContext
 import com.example.subsinthe.crossline.network.VertxSocketFactory as SocketFactory
 import com.example.subsinthe.crossline.soulseek.Client as SoulseekClient
 
+private class DefaultExceptionHandler {
+    companion object {
+        fun get() = CoroutineExceptionHandler { _: CoroutineContext, ex: Throwable ->
+            val stackTrace = ex.stackTrace.fold("") { trace, frame -> "$trace\n$frame" }
+            LOG.severe("Uncaught exception: $ex:$stackTrace")
+        }
+
+        private val LOG = loggerFor<DefaultExceptionHandler>()
+    }
+}
+
 private class UiScope : CoroutineScope {
-    override val coroutineContext: CoroutineContext = Dispatchers.Main
+    override val coroutineContext: CoroutineContext = Dispatchers.Main + DefaultExceptionHandler.get()
 }
 
 private class IoScope : CoroutineScope {
-    override val coroutineContext: CoroutineContext = Dispatchers.IO
+    override val coroutineContext: CoroutineContext = Dispatchers.IO + DefaultExceptionHandler.get()
 }
 
 class MainActivity : AppCompatActivity() {
