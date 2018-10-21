5b83d92859f8b59515517ecf49043a091becf2c3
subsinthe <subsinthe@gmail.com>
2018-10-21 20:39:43 +0300

    Some COOL upgrades for AsyncIterator

--- app/src/main/java/com/example/subsinthe/crossline/util/AsyncIterator.kt
+++ app/src/main/java/com/example/subsinthe/crossline/util/AsyncIterator.kt
@@ -1,13 +1,14 @@
 package com.example.subsinthe.crossline.util
 
 import kotlinx.coroutines.channels.ReceiveChannel
+import java.io.Closeable
 
 class AsyncIterator<T>(val iterator: ReceiveChannel<T>) {
-    inline fun <R> use(block: (ReceiveChannel<T>) -> R): R {
-        try {
-            return block(iterator)
-        } finally {
-            iterator.cancel()
-        }
+    val _closeable = object : Closeable { override fun close() { iterator.cancel() } }
+
+    inline fun <R> use(block: (ReceiveChannel<T>) -> R) = _closeable.use { block(iterator) }
+
+    inline fun <R> closeOnError(block: (ReceiveChannel<T>) -> R) = _closeable.closeOnError {
+        block(iterator)
     }
 }
