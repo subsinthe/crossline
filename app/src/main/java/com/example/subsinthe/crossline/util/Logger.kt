2cba03e7fc0e8ef0bd6958624c1b92e9d8cd1fb5
subsinthe <subsinthe@gmail.com>
2018-10-24 23:55:05 +0300

    util.Logger: Fix build

--- app/src/main/java/com/example/subsinthe/crossline/util/Logger.kt
+++ app/src/main/java/com/example/subsinthe/crossline/util/Logger.kt
@@ -5,7 +5,7 @@ import java.util.logging.Logger
 inline fun <reified T : Any> loggerFor() = Logger.getLogger(T::class.java.name)
 
 inline fun <R> Logger.try_(message: String, block: () -> R?): R? {
-    return try_({ message }, block())
+    return try_({ message }, block)
 }
 
 inline fun <R> Logger.try_(lazyMessage: () -> String, block: () -> R?): R? {
