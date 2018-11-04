c1fbdd698029208d0236ee6a3e12b3d3d4cc0286
subsinthe <subsinthe@gmail.com>
2018-11-04 16:52:20 +0300

    util.CoroutineScope: Introduce setTimer

--- app/src/main/java/com/example/subsinthe/crossline/util/CoroutineScope.kt
+++ app/src/main/java/com/example/subsinthe/crossline/util/CoroutineScope.kt
@@ -1,8 +1,20 @@
 package com.example.subsinthe.crossline.util
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+
+class CoroutineScopeLogger {
+    companion object { val LOG = loggerFor<CoroutineScopeLogger>() }
+}
 
 fun <T> CoroutineScope.pack(callable: suspend (T) -> Unit) = {
     it: T -> launch { callable(it) }.discard()
+}
+
+fun CoroutineScope.setTimer(interval: Long, body: suspend () -> Unit) = launch {
+    while (true) {
+        CoroutineScopeLogger.LOG.try_("Uncaught exception in timer") { body() }
+        delay(interval)
+    }
 }
